3df9642e86f9dcbb9bbed2875998418e3f032684
Wojciech Niestrój <wojciech.niestroj@billennium.com>
2019-02-21 18:23:30 +0100

    searchbox works

--- zad 3/zad3/src/app/app.component.ts
+++ zad 3/zad3/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { tree_token } from '../assets/mock_tree.js';
 export class AppComponent implements OnInit {
   title = 'zad3';
   openArray = [];
-  itemvalue: string;
+  search = '';
 
   constructor (
     @Inject(tree_token) private tree_: any[]
@@ -24,12 +24,38 @@ export class AppComponent implements OnInit {
   }
 
   fn(item) {
+    console.log('1')
     if (this.openArray.indexOf(item) !== -1) {
       this.openArray.splice(this.openArray.indexOf(item), 1);
     } else {
       this.openArray.push(item);
     }
     document.getElementById('array').innerText = this.openArray.toString();
+  }
+
+  getvalue(){
+    this.search = document.getElementById('search').value;
+  }
+
+  checkParent(itemvalue) {
+    if (this.search === '') {
+      return true;
+    }
+    if(this.search.toLocaleLowerCase().startsWith(itemvalue.toString().toLocaleLowerCase())) {
+      return true;
+    }
+    return false;
+  }
+
+  checkChildren(itemvalue) {
+
+    if (this.search === '') {
+      return true;
+    }
+    if(itemvalue.toString().toLocaleLowerCase().includes(this.search.toLocaleLowerCase()) {
+      return true;
+    }
+    return false;
   }
 }
 
