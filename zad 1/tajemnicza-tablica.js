0c86c7be9ea12e78f735fb4a5778191cd870451b
Wojciech Niestrój <wojciech.niestroj@billennium.com>
2019-02-21 12:52:06 +0100

    more refractoring

--- zad 1/tajemnicza-tablica.js
+++ zad 1/tajemnicza-tablica.js
@@ -6,7 +6,7 @@ function odkryj() {
             billenium_string += allTdElements[i].innerText;
         }
     }
-    document.getElementById("result").innerText = billenium_string;
+    change_inner_text("result", billenium_string);
 }
 
 function show() {
@@ -19,7 +19,7 @@ function show() {
             allTdElements[i].style.backgroundColor = "white";
             allTdElements[i].style.color = "white";
         }
-        document.getElementById("result").innerText = "";
+        change_inner_text("result", "")
     }
 }
 
@@ -28,7 +28,7 @@ function rearrange() {
     const allTdElements = document.getElementsByTagName("td");
     let temp = []
     for (let i = allTdElements.length - 1; i >= 0; i--){
-        if(getComputedStyle(allTdElements[i]).color === getComputedStyle(allTdElements[i]).backgroundColor) {
+        if (getComputedStyle(allTdElements[i]).color === getComputedStyle(allTdElements[i]).backgroundColor) {
             allTdElements[i].parentNode.removeChild(allTdElements[i]);
         }  else {
             let temp_elem = allTdElements[i].cloneNode();
@@ -39,7 +39,15 @@ function rearrange() {
         for (let i = temp.length - 1; i >= 0; i--){
             selectedTr.parentNode.append(temp[i]);
         }
-        document.getElementById("odkryj").disabled = true;
-        document.getElementById("show").disabled = true;
+        disable_buttons();
     }
+}
+
+function disable_buttons() {
+    document.getElementById("odkryj").disabled = true;
+    document.getElementById("show").disabled = true;
+}
+
+function change_inner_text(id, value) {
+    document.getElementById(id).innerText = value;
 }
