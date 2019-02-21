15374027b7cf7bd167e570b2d7282b4ba1ad6a8e
Wojciech Niestrój <wojciech.niestroj@billennium.com>
2019-02-21 12:55:47 +0100

    even more refractoring

--- zad 1/tajemnicza-tablica.js
+++ zad 1/tajemnicza-tablica.js
@@ -26,19 +26,17 @@ function show() {
 function rearrange() {
     const selectedTr = document.getElementById("wrap")
     const allTdElements = document.getElementsByTagName("td");
-    let temp = []
+    let nodes_array = []
     for (let i = allTdElements.length - 1; i >= 0; i--){
         if (getComputedStyle(allTdElements[i]).color === getComputedStyle(allTdElements[i]).backgroundColor) {
             allTdElements[i].parentNode.removeChild(allTdElements[i]);
         }  else {
             let temp_elem = allTdElements[i].cloneNode();
             temp_elem.innerText = allTdElements[i].innerText;
-            temp.push(temp_elem);
+            nodes_array.push(temp_elem);
             allTdElements[i].parentNode.removeChild(allTdElements[i]);
         }
-        for (let i = temp.length - 1; i >= 0; i--){
-            selectedTr.parentNode.append(temp[i]);
-        }
+        append_to_element_desc(selectedTr, nodes_array);
         disable_buttons();
     }
 }
@@ -50,4 +48,10 @@ function disable_buttons() {
 
 function change_inner_text(id, value) {
     document.getElementById(id).innerText = value;
+}
+
+function append_to_element_desc(element, array) {
+    for (let i = array.length - 1; i >= 0; i--){
+        element.parentNode.append(array[i]);
+    }
 }
